113a294b280415178f4ccb35618fb9d7e28ab9e9
RuRo <r3@ihatebot.ru>
2019-03-26 11:05:32 +0200

    addint test for empty image

--- scanner_test.go
+++ scanner_test.go
@@ -6,9 +6,33 @@ import (
 	"image/png"
 	"os"
 	"testing"
-
 	//iconv "github.com/djimenez/iconv-go"
 )
+
+func TestNoImage(t *testing.T) {
+	f, err := os.Open("testdata/empty.jpg")
+	if err != nil {
+		t.Fail()
+		return
+	}
+
+	i, _ := jpeg.Decode(f)
+
+	img := FromImage(i)
+
+	s := NewScanner()
+	s.SetConfig(0, CFG_ENABLE, 1)
+
+	status := s.Scan(img)
+	defer s.Destroy()
+
+	// check if we found something
+	if status != 0 {
+		t.Fail()
+		return
+	}
+
+}
 
 func TestBarcode(t *testing.T) {
 	f, err := os.Open("testdata/barcode.png")
@@ -25,8 +49,14 @@ func TestBarcode(t *testing.T) {
 	s := NewScanner()
 	s.SetConfig(0, CFG_ENABLE, 1)
 
-	s.Scan(img)
+	status := s.Scan(img)
 	defer s.Destroy()
+
+	// check if we found something
+	if status != 1 {
+		t.Fail()
+		return
+	}
 
 	img.First().Each(func(str string) {
 		fmt.Println(str)
@@ -50,8 +80,14 @@ func TestQRCode(t *testing.T) {
 
 	s := NewScanner()
 	s.SetConfig(0, CFG_ENABLE, 1)
-	s.Scan(img)
+	status := s.Scan(img)
 	defer s.Destroy()
+
+	// check if we found something
+	if status != 1 {
+		t.Fail()
+		return
+	}
 
 	img.First().Each(func(str string) {
 		// Charset decoding
@@ -59,14 +95,14 @@ func TestQRCode(t *testing.T) {
 		//str, _ = iconv.ConvertString(str, "iso-8859-1", "utf-8")
 		fmt.Println(str)
 
-		if str != "ZBar big law good! ZBar大法好!" {
+		if str != "ZBar big law good! ZBar螟ｧ豕募･ｽ!" {
 			t.Fail()
 		}
 	})
 }
 
 func TestPhoto(t *testing.T) {
-	f, err := os.Open("/mnt/sdcard/Download/sessfo-qrcode.jpg")
+	f, err := os.Open("testdata/photo.jpg")
 
 	if err != nil {
 		t.Fail()
@@ -78,8 +114,14 @@ func TestPhoto(t *testing.T) {
 
 	s := NewScanner()
 	s.SetConfig(0, CFG_ENABLE, 1)
-	s.Scan(img)
+	status := s.Scan(img)
 	defer s.Destroy()
+
+	// check if we found something
+	if status != 1 {
+		t.Fail()
+		return
+	}
 
 	img.First().Each(func(str string) {
 		// Charset decoding
